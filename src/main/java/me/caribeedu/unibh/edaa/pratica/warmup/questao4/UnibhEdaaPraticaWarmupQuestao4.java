4b8051aa66fa5fbea68b57841e6a3a494b5e3834
Edu Caribé <edu.caribe@.hotmail.com>
2022-08-29 13:29:13 -0300

    feat: executar questoes 4 e 5

--- src/main/java/me/caribeedu/unibh/edaa/pratica/warmup/questao4/UnibhEdaaPraticaWarmupQuestao4.java
+++ src/main/java/me/caribeedu/unibh/edaa/pratica/warmup/questao4/UnibhEdaaPraticaWarmupQuestao4.java
@@ -4,12 +4,97 @@
  */
 package me.caribeedu.unibh.edaa.pratica.warmup.questao4;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Edu Caribé
  */
 public class UnibhEdaaPraticaWarmupQuestao4 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Roubo Banco HSVC");
+        
+        var valoresSuspeitos = obterValoresSuspeitos();
+        var valorAssaltanteConfesso = obterValorAssaltanteConfessado();
+                
+        var assaltantes = avaliarSuspeitos(valoresSuspeitos, valorAssaltanteConfesso);
+        int valorTotalRoubado = 0;
+        
+        for(int i=0; i<assaltantes.size(); i++) {
+            int indiceAssaltante = assaltantes.get(i);
+            
+            System.out.println("O suspeito de nº " + (indiceAssaltante + 1) + " é um dos assaltantes.");
+            
+            valorTotalRoubado += valoresSuspeitos[indiceAssaltante];
+        }
+        
+        System.out.println();
+        System.out.print("Ao todo os assaltantes roubaram R$" + valorTotalRoubado + ",00.");
+    }
+    
+    private static int[] obterValoresSuspeitos() {
+        final Scanner input = new Scanner(System.in);
+        
+        var valores = new int[10];
+        int suspeitosContabilizados = 0;
+        
+        while(suspeitosContabilizados < 10) {
+            int suspeitoNumero = suspeitosContabilizados + 1;
+            
+            System.out.print("Insira o valor contido pelo suspeito nº " + suspeitoNumero + ": ");
+            int valorContido = input.nextInt();
+            
+            if (valorContido < 0) {
+                System.out.println();
+                System.out.print("Favor inserir um valor válido. Tente novamente.");
+                continue;
+            }
+            
+            valores[suspeitosContabilizados] = valorContido;
+            suspeitosContabilizados++;
+            
+            System.out.println("O suspeito de nº " + suspeitosContabilizados + " continha R$" + valorContido + ",00.");
+        }
+        
+        return valores;
+    }
+    
+    private static int obterValorAssaltanteConfessado() {
+        System.out.println();
+        
+        final Scanner input = new Scanner(System.in);
+        int valorConfessado = 0;
+        
+        while(valorConfessado == 0) {
+            System.out.print("Insira o valor contido pelo assaltante confessado: ");
+            int valorContido = input.nextInt();
+
+            if (valorContido <= 0 || valorContido % 10 != 0) {
+                System.out.println();
+                System.out.print("Favor inserir um valor válido. Os assaltantes continham apenas notas de 10.");
+                continue;
+            }
+            
+            System.out.println();
+            valorConfessado = valorContido;
+        }
+        
+        return valorConfessado;
+    }    
+    
+    private static ArrayList<Integer> avaliarSuspeitos(int[] valoresSuspeitos, int valorAssaltanteConfesso) {
+        var indicesSuspeitosQueSaoAssaltantes = new ArrayList<Integer>();
+        
+        for(int i=0; i<valoresSuspeitos.length; i++) {
+            var valorContido = valoresSuspeitos[i];
+            
+            if (valorContido % 10 != 0 || valorContido < valorAssaltanteConfesso)
+                continue;
+            
+            indicesSuspeitosQueSaoAssaltantes.add(i);
+        }
+        
+        return indicesSuspeitosQueSaoAssaltantes;
     }
 }
--- src/main/java/me/caribeedu/unibh/edaa/pratica/warmup/questao5/UnibhEdaaPraticaWarmupQuestao5.java
+++ src/main/java/me/caribeedu/unibh/edaa/pratica/warmup/questao5/UnibhEdaaPraticaWarmupQuestao5.java
@@ -4,12 +4,36 @@
  */
 package me.caribeedu.unibh.edaa.pratica.warmup.questao5;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Edu Caribé
  */
 public class UnibhEdaaPraticaWarmupQuestao5 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final Scanner input = new Scanner(System.in);
+        
+        int[] numerosEscritaUnica = new int[5];
+        boolean[] numerosEscritos = new boolean[5];
+        
+        int indiceNumeroAtual = 0;
+        
+        while(true) {
+            System.out.println("Insira um valor para o " + (indiceNumeroAtual + 1) + "º número: ");
+            var numeroInserido = input.nextInt();
+            
+            if (numerosEscritos[indiceNumeroAtual] != true) {
+                numerosEscritaUnica[indiceNumeroAtual] = numeroInserido;
+                numerosEscritos[indiceNumeroAtual] = true;
+            }
+            
+            System.out.println("O valor atual do " + (indiceNumeroAtual + 1) + "º número é " + numerosEscritaUnica[indiceNumeroAtual] + ".");
+            
+            if (indiceNumeroAtual < 4)
+                indiceNumeroAtual++;
+            else
+                indiceNumeroAtual = 0;
+        }
     }
 }
